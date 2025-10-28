aea949b1a9fd83f26c9da50d3f30103cc84eca86
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2025-10-27 22:09:19 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const path = require('path')
+//const Modelo = require('./modelo.js')
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(express.static('public'))
+app.set('view engine','ejs');
+
+// Agregado para direccionar el servidor---------------
+var estaUrl = path.join(__dirname);
+
+let produccion = false
+if(estaUrl[0] == "C" && estaUrl[1] == ":"){
+    produccion = false;
+}else{
+    produccion = true;
+}
+
+if(produccion){
+    _url = "https://"+process.env.RAILWAY_PUBLIC_DOMAIN+"/";
+}else{
+    _url = "http://localhost:3000/";
+}
+console.log("produccion")
+console.log(produccion)
+console.log("_url")
+console.log(_url)
+
+//-------------------------------------------------------
+
+
+
+app.get('/', (req, res)=>{
+    //res.send(`<p>Respondiendo desde puerto ${PORT}`)
+    res.render('index.ejs',{url: _url})
+})
+
+
+
+const PORT = 3000;
+app.listen(PORT, ()=>{console.log(`Escuchando en puerto ${3000}`)})
